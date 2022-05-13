8137a48a1d4d896b81cf5f495daa5af57f9c9e22
mpopovs <maris@popovs.eu>
2022-05-13 23:10:31 +0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,16 @@
-// svelte.config.js
-import adapter from '@sveltejs/adapter-node';
+/** @type {import('@sveltejs/kit').Config} */
 
-export default {
-  kit: {
-    adapter: adapter()
-  }
+import node from '@sveltejs/adapter-node';
+
+const config = {
+	kit: {
+		adapter: node(),
+		
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		}
+	}
 };
+
+export default config;
