2d8a020666f61d57e620942c7955372e31a3fced
mpopovs <maris@popovs.eu>
2022-07-07 20:41:14 +0300

    chat
    fb chat

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -27,7 +27,24 @@ onMount(async () => {
       });
     });
    
-   
+    var chatbox = document.getElementById('fb-customer-chat');
+  chatbox.setAttribute("page_id", "101880118977330");
+  chatbox.setAttribute("attribution", "biz_inbox");
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v14.0'
+    });
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
  
 </script>
 
@@ -76,6 +93,16 @@ onMount(async () => {
 </Row>
 </Grid>
 
+<!-- Messenger Chat Plugin Code -->
+<div id="fb-root"></div>
+
+<!-- Your Chat Plugin code -->
+<div id="fb-customer-chat" class="fb-customerchat">
+</div>
+
+
+
+<!-- Your SDK code -->
 
 
 
